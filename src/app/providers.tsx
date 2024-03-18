82b03f4c215d6ae9b492c5dd94533dd059622487
Shawn Kost <shawnmkost@gmail.com>
2024-03-17 22:53:24 -0700

    Adjust api host

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -5,7 +5,7 @@ import { PostHogProvider } from "posthog-js/react";
 
 if (typeof window !== "undefined") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    api_host: "https://shawnkost.dev/ingest",
+    api_host: "https://www.shawnkost.dev/ingest",
     ui_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     capture_pageview: false, // Disable automatic pageview capture, as we capture manually
   });
