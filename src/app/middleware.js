819918e38928fd91ae14238127ded8c5c4dfd2bc
Shawn Kost <shawnmkost@gmail.com>
2024-04-22 22:15:00 -0700

    Remove js check and fix eslint error

--- src/app/middleware.js
+++ src/app/middleware.js
@@ -1,11 +1,12 @@
 import { NextResponse } from 'next/server'
 
 export function middleware(request) {
+  let url = request.nextUrl.clone()
   const hostname = url.pathname.startsWith("/ingest/static/") ? 'us-assets.i.posthog.com' : 'us.i.posthog.com'
   const requestHeaders = new Headers(request.headers)
+
   requestHeaders.set('host', hostname)
 
-  let url = request.nextUrl.clone()
   url.protocol = 'https'
   url.hostname = hostname
   url.port = 443
