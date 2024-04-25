a291b6b7428cc5d6c8840c1493e93cea7d5003c4
Shawn Kost <shawnmkost@gmail.com>
2024-04-25 11:45:26 -0700

    Format files

--- src/app/middleware.js
+++ src/app/middleware.js
@@ -1,22 +1,24 @@
-import { NextResponse } from 'next/server'
+import { NextResponse } from "next/server";
 
 export function middleware(request) {
-  let url = request.nextUrl.clone()
-  const hostname = url.pathname.startsWith("/ingest/static/") ? 'us-assets.i.posthog.com' : 'us.i.posthog.com'
-  const requestHeaders = new Headers(request.headers)
+  let url = request.nextUrl.clone();
+  const hostname = url.pathname.startsWith("/ingest/static/")
+    ? "us-assets.i.posthog.com"
+    : "us.i.posthog.com";
+  const requestHeaders = new Headers(request.headers);
 
-  requestHeaders.set('host', hostname)
+  requestHeaders.set("host", hostname);
 
-  url.protocol = 'https'
-  url.hostname = hostname
-  url.port = 443
-  url.pathname = url.pathname.replace(/^\/ingest/, '');
+  url.protocol = "https";
+  url.hostname = hostname;
+  url.port = 443;
+  url.pathname = url.pathname.replace(/^\/ingest/, "");
 
   return NextResponse.rewrite(url, {
     headers: requestHeaders,
-  })
+  });
 }
 
 export const config = {
-  matcher: '/ingest/:path*',
-}
+  matcher: "/ingest/:path*",
+};
