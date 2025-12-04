ffc19ac7be24dfe0996ce0c2e5d780d93e522e71
Shawn Kost <shawnmkost@gmail.com>
2025-12-03 16:40:54 -0800

    Add umami tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,18 @@ import { Contact } from "@/app/ui/Contact/Contact";
 import { NavBar } from "@/app/ui/Navbar/Navbar";
 import MyProjects from "@/app/ui/Projects/MyProjects";
 import MySkills from "@/app/ui/Skills/MySkills";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <script
+          defer
+          src="https://cloud.umami.is/script.js"
+          data-website-id="fcefbfa2-de35-4c1c-8247-d8cd31747fd6"
+        ></script>
+      </Head>
       <NavBar />
       <main>
         <div className="home-gradient bg-cover bg-center bg-no-repeat">
